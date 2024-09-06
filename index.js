a41b8104a8fba349f572bc7a0ed09f21c292d195
Valentin <valentincaminos12345@gmail.com>
2024-09-06 19:35:44 -0300

    bug fix

--- index.js
+++ index.js
@@ -17,50 +17,6 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   })
 })
-/* 
-document.addEventListener('DOMContentLoaded', function () {
-  var formulario = document.querySelector('form')
-
-  formulario.addEventListener('submit', function (event) {
-    event.preventDefault()
-
-    var nombre = document.getElementById('nombre').value.trim()
-    var correo = document.getElementById('correo').value.trim()
-    var mensaje = document.getElementById('mensaje').value.trim()
-
-    if (nombre === '' || correo === '' || mensaje === '') {
-      alert('Por favor, completa todos los campos.')
-      return
-    }
-
-    enviarDatos(nombre, correo, mensaje)
-  })
-
-  function enviarDatos(nombre, correo, mensaje) {
-    fetch('tu_script_de_procesamiento.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ nombre, correo, mensaje }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Hubo un problema con la solicitud.')
-        }
-        return response.json()
-      })
-      .then((data) => {
-        console.log('Datos enviados correctamente:', data)
-        alert('¡Datos enviados correctamente!')
-      })
-      .catch((error) => {
-        console.error('Error al enviar datos:', error)
-        alert('Hubo un problema al enviar los datos.')
-      })
-  }
-}) */
-
 
 const typed = new Typed('.typed', {
   strings: ['Y ESTO ES LO QUE HAGO.'],
